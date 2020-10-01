3124b8e7bad689d8155985b1ae1a13ff222be408
omega <omega@wtf.com>
2020-10-01 11:32:40 +0200

    [Core] Initial commit.

---
+++ omgCarSearch/omgCarSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  omgCarSearchApp.swift
+//  omgCarSearch
+//
+//  Created by Julien BOULLY on 01/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct omgCarSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
